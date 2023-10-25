63e8637cd0e91aee64657c2a18e4d791c88171a8
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-25 21:10:13 +0530

    updates

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import router from './Routes/index.js';
+import { Hello } from './Controllers/GlobalControllers.js';
 
 const app = express()
+
+// app.use((req, res, next) => {
+//     console.log("hi from middleware use")
+//     //res.send("hii from middleware use")
+//     next();
+// })
 
 app.get("/", function(req, res) {
     res.send("Hello Yogesh..")
 
 })
+
+app.get("/hello", Hello);
 
 app.use("/api/v1", router)
 
