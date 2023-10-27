1349ea2c2f19a6b6582311435c75d536cc0b0560
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-27 22:11:44 +0530

    updates

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import router from './Routes/index.js';
 import { Hello } from './Controllers/GlobalControllers.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express()
+
+dotenv.config();
 
 // app.use((req, res, next) => {
 //     console.log("hi from middleware use")
@@ -18,6 +22,8 @@ app.get("/", function(req, res) {
 app.get("/hello", Hello);
 
 app.use("/api/v1", router)
+
+mongoose.connect(process.env.MONGOURL).then(() => console.log('Database connected.'))
 
 
 app.listen(8000, () => console.log("App is running on port 8000."))
