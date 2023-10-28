ef2dd2b43ddd2bb787765c3e07321f52a9e26262
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-29 00:03:41 +0530

    updates

--- Routes/Auth.routes.js
+++ Routes/Auth.routes.js
@@ -3,7 +3,7 @@ import { Login, Register } from "./../Controllers/Auth.controllers.js"
 
 const router = Router();
 
-router.post("/login", Login)
+router.get("/login", Login)
 router.post("/register", Register)
 
 export default router;
