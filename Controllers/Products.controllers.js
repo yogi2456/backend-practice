21b5861f13b2d3e63e25908f163c22dc0857e686
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-04 13:37:25 +0530

    updates

--- Controllers/Products.controllers.js
+++ Controllers/Products.controllers.js
@@ -9,14 +9,14 @@ export const getSingleProduct = (req, res) => {
 
 export const addProduct = async (req, res) => {
     try{
-        const {name, price, category, image} = req.body;
-        if (!name || !price || !category || !image) return res.status(401).json({success: false, message: "All fields are mandatory"})
+        const {name, price, category, image, id} = req.body;
+        if (!name || !price || !category || !image || !id) return res.status(401).json({success: false, message: "All fields are mandatory"})
         const product = new ProductModal({
-            name, price, category, image: image
+            name, price, category, image: image, userId: id
         })
-          console.log(product, "- product here")
+          //console.log(product, "- product here")
         const ress = await product.save();
-          console.log(ress, "response from mongodb")
+          //console.log(ress, "response from mongodb")
         return res.status(200).json({success: true, message: "Product added successfully"})
 
     } catch (error) {
--- Modals/Product.modal.js
+++ Modals/Product.modal.js
@@ -4,7 +4,11 @@ const product = new Schema({
     name: String,
     price: Number,
     image: String,
-    category: String
+    category: { type: String },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
 })
 
 export default mongoose.model('Product', product)
